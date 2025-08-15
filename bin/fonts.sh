f718371d8dce576db290dc7f3b864112117c77aa
Timo Suoranta <timo.suoranta@supercell.com>
2025-08-15 13:58:22 +0300

    mac fonts, bc5 glsl

---
+++ bin/fonts.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+brew tap homebrew/cask-fonts
+brew install --cask font-cascadia-code
+brew install --cask font-cascadia-code-pl
+brew install --cask font-cascadia-mono
+brew install --cask font-cascadia-mono-pl
+
